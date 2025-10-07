d8344df60312ef4471104aa37fdd20f022c52497
FikalAhmad <fikaalm26@gmail.com>
2025-10-07 18:40:34 +0700

    change logo

--- src/pages/homepage/components/Navbar.tsx
+++ src/pages/homepage/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="flex justify-between items-center h-20 px-10">
       <div className="flex flex-col">
         <h2 className="text-2xl font-bold text-maroon">Fotoboothgaksi</h2>
-        <span className="text-[1px]"> Created by KAL 🖤</span>
+        <span className="text-[1px]"> Created by Suka Kopi Manis ☕</span>
       </div>
       <Button className="bg-maroon" asChild>
         <Link to={"/capture"}>Try it Now 📸</Link>
